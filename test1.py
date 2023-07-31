98cb67c99ebd00712158b6327a974d0c5c4fa26e
ThibaultDP <thibault.de.paepe@skyline.be>
2023-07-31 17:49:32 +0200

    nieuw dingen

--- test1.py
+++ test1.py
@@ -3,9 +3,11 @@ def main():
         # Vraag de gebruiker om de twee getallen in te voeren
         num1 = float(input("Voer het eerste getal in: "))
         num2 = float(input("Voer het tweede getal in: "))
+        num3 = float(input("Voer het tweede getal in: "))
+
         
         # Bereken de som van de twee getallen
-        result = num1 + num2
+        result = num1 + num2 + num3
         
         # Toon het resultaat aan de gebruiker
         print("De som van {} en {} is: {}".format(num1, num2, result))
